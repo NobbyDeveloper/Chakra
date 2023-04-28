9384fe7234047590f4edc9e420c9795086dc0171
Syraxes/Vilictus <101947195+Vilictus@users.noreply.github.com>
2023-04-28 14:57:51 -0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles/main.css";
+import { Box, Flex, Text, Heading } from "@chakra-ui/core";
+
+export default function App() {
+  return (
+    <Flex justifyContent="center" alignItems="center" minHeight="100vh">
+      <Heading fontFamily="Clash Display" fontWeight="semibold">
+        Daku Softworks
+      </Heading>
+      <Text></Text>
+    </Flex>
+  );
+}
